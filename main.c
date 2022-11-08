7db981a9b97b50e4ff97af1dcdd2eb219b9712aa
ppaksso <113421378+ppaksso@users.noreply.github.com>
2022-11-08 17:13:10 +0900

    06

--- main.c
+++ main.c
@@ -4,23 +4,21 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	FILE*fp;
-	char str[30];
+	FILE*fp=NULL;
+	char c;
+	char str [30];
 	
-	fp=fopen("sample1.txt","w");
+	fp=fopen("sample1.txt","r");
 	
-	printf("input a word");
-	scanf("%s",str);
-	fprintf(fp,"%s\n",str);
-	
-	printf("input a word");
-	scanf("%s",str);
-	fprintf(fp,"%s\n",str);
-	
-	printf("input a word");
-	scanf("%s",str);
-	fprintf(fp,"%s\n",str);
+
+	/*while( (c=fgetc(fp))!= EOF)
+	  putchar(c);
+	  */
+	  
+	while (fgets(str,30,fp) !=NULL)
+	  printf("%s",str); 
 	
 	fclose(fp);
+	
 	return 0;
 } 
